3a6d803a9ee7a16d81578852bcf62ebf49f1e366
Michael <greenmr95@hotmail.com>
2022-10-30 22:59:02 +0000

    hundreds added

--- hundreds.js
+++ hundreds.js
@@ -1,3 +1,15 @@
 
 
 
+function howManyHundreds(bottles) {
+  let containers = bottles / 100
+  let wholeBottles = bottles - (bottles % 100)
+  let wholeContainers = wholeBottles / 100 
+  if (bottles % 100 === 0) {
+    return containers
+  } else {
+    return wholeContainers
+  }  
+}
+
+console.log(howManyHundreds(894))
