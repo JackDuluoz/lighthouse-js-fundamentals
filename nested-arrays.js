c6923947c9f56279bcdb8c666f8f796725ffda9f
Michael <greenmr95@hotmail.com>
2022-10-31 03:42:20 +0000

    ending with arrays tonight

---
+++ nested-arrays.js
@@ -0,0 +1,25 @@
+
+
+
+let gridNumbers = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16]
+]
+
+for (let row = 0; row < gridNumbers.length; row++) {
+  for (let column = 0; column < gridNumbers[row].length; column++) {
+    console.log(gridNumbers[row][column])
+  }
+}
+
+console.log(gridNumbers[0][2])
+
+
+
+ /*   for (let column = 0; column < gridNumbers[row].length; column++) {
+      console.log(gridNumbers[row][column])
+    }
+  }
+  */
