5e611a9406ecbad98c6ac9cc1a8e3d486dcaa475
Michael <greenmr95@hotmail.com>
2022-11-03 22:27:36 +0000

    switching to nodeschool

--- concatenate-arrays.js
+++ concatenate-arrays.js
@@ -7,14 +7,11 @@ function concat(data1, data2) {
   }
   for (let second = 0; second < data2.length; second++) {
     combined.push(data2[second])
-  }
-  
-  return combined
+  }  
+    return combined
 }
 
-
-
-  console.log(concat([1, 2, 3], [4, 5, 6]))
-  console.log(concat([0, 3, 1], [9, 7, 2]))
-  console.log(concat([], [9, 7, 2]))
-  console.log(concat([5, 10], []))
+console.log(concat([1, 2, 3], [4, 5, 6]))
+console.log(concat([0, 3, 1], [9, 7, 2]))
+console.log(concat([], [9, 7, 2]))
+console.log(concat([5, 10], []))
